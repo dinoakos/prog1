bf5d47c1748fd4b201577bd15e026849c1535c15
dinoakos <43711534+dinoakos@users.noreply.github.com>
2022-03-04 18:21:33 +0100

    Update Drill2.cpp

--- Drill2.cpp
+++ Drill2.cpp
@@ -1,4 +1,22 @@
-#include "../GUI/Simple_window.h" #include "../GUI/Graph.h" #include "./Arc.h" #include <string> #include <iostream> using namespace Graph_lib; int main() try { // Window Point tl {100, 100}; Simple_window win {tl, 600, 400, "Chapter 13 Ex 1"}; // Reference Circle c {Point{300, 200}, 75}; c.set_color(Color::yellow); win.attach(c); // Arc tests Arc a1 {Point{300, 200}, 50, 0, 90}; a1.set_color(Color::blue); win.attach(a1); Arc a2 {Point{300, 200}, 100, 90, 180}; a2.set_color(Color::red); win.attach(a2); Arc a3 {Point{300, 200}, 150, 90, 360}; a3.set_color(Color::green); win.attach(a3); Arc a4 {Point{300, 200}, 100, 270, 360}; a4.set_color(Color::red); win.attach(a4); win.wait_for_button(); 
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+#include "./Arc.h"
+#include <string>
+#include <iostream>
+using namespace Graph_lib;
+int main() try {
+Point tl {100, 100};
+Simple_window win {
+tl, 600, 400, "Chapter 13 Ex 1"}; 
+Circle c {Point{300, 200}, 75};
+c.set_color(Color::yellow);
+win.attach(c);
+Arc a1 {Point{300, 200}, 50, 0, 90};
+a1.set_color(Color::blue);
+win.attach(a1);
+Arc a2 {Point{300, 200}, 100, 90, 180};
+a2.set_color(Color::red);
+win.attach(a2); Arc a3 {Point{300, 200}, 150, 90, 360}; a3.set_color(Color::green); win.attach(a3); Arc a4 {Point{300, 200}, 100, 270, 360}; a4.set_color(Color::red); win.attach(a4); win.wait_for_button(); 
 //2
 const Point tl {100, 100}; Simple_window win {tl, 600, 400, "Chapter 13 Ex 2"}; Box b1 {Point{200, 150}, 200, 100, 20}; b1.set_color(Color::red); win.attach(b1); Box b2 {Point{100, 100}, 120, 80, 10}; b2.set_style(Line_style{Line_style::solid,4}); b2.set_color(Color::blue); win.attach(b2); Box b3 {Point{400, 300}, 50, 50, 25}; win.attach(b3); win.wait_for_button();
 //3
